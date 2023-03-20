586cf940371fee0e232f245472ab0a52a2ae2f6f
m-njora <mmuthekijora@gmail.com>
2023-03-20 20:09:55 +0300

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,32 +3,43 @@ Pizza.destroy_all
 Restaurant.destroy_all
 
 puts "seeding pizzas 🍕...."
-# Seed data for pizzas table
-10.times do
-    Pizza.create(
-      name: Faker::Food.dish,
-      ingredients: Faker::Food.ingredient,
-      price: Faker::Commerce.price(range: 1..30.0, as_string: false)
-    )
-  end
+# # Seed data for pizzas table
+# 10.times do
+#     Pizza.create(
+#       name: Faker::Food.dish,
+#       ingredients: Faker::Food.ingredient,
+#       price: Faker::Commerce.price(range: 1..30.0, as_string: false)
+#     )
+#   end
   
-# Seed data for restaurant_pizzas table
-10.times do
-  RestaurantPizza.create(
-    "pizza_id": rand(1..10),
-    "restaurant_id": rand(1..10),
-    name: Faker::Food.dish,
-    price: Faker::Commerce.price(range: 1..30.0, as_string: false)
-  )
-end
+# # Seed data for restaurant_pizzas table
+# 10.times do
+#   RestaurantPizza.create(
+#     "pizza_id": rand(1..10),
+#     "restaurant_id": rand(1..10),
+#     name: Faker::Food.dish,
+#     price: Faker::Commerce.price(range: 1..30.0, as_string: false)
+#   )
+# end
 
   
-  # Seed data for restaurants table
-  10.times do
-    Restaurant.create(
-      name: Faker::Restaurant.name,
-      address: Faker::Address.full_address,
-    )
-  end
+10.times do
+  res= Restaurant.create(
+          "name": Faker::Restaurant.name,
+          "address": Faker::Address.full_address
+      )
+  
+     piz = Pizza.create(
+          "name": Faker::Food.dish,
+          "ingredients": Faker::Food.ingredient
+      )
+      rand(3..5).times do
+        RestaurantPizza.create(
+            "price": rand(1.0..29.0),
+            "pizza_id": rand(1..10),
+            "restaurant_id": rand(1..10)
+        )
+      end
+    end    
 
 puts "done seeding"
